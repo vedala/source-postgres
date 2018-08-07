c31ebb4db8b91180439474f199d1be35af58ba02
Sundaram Vedala <svedala23@gmail.com>
2018-08-07 13:31:34 -0400

    Fetch from database instead of serving dummy data

--- source_postgres/__init__.py
+++ source_postgres/__init__.py
@@ -1,33 +1,24 @@
 name = "source_postgres"
 
+import psycopg2
+
 class ChompSourceException(Exception):
     pass
 
 class SourcePostgres(object):
-    dummy_data = [(10000001, "AAAAAAAAAA"),
-                  (10000002, "AAAAAAAAAB"),
-                  (10000003, "AAAAAAAAAC"),
-                  (10000004, "AAAAAAAAAD"),
-                  (10000005, "AAAAAAAAAE"),
-                  (10000006, "AAAAAAAAAF"),
-                  (10000007, "AAAAAAAAAG"),
-                  (10000008, "AAAAAAAAAH"),
-                  (10000009, "AAAAAAAAAI")
-                  ]
-
-    batchsize = 2
 
     def __init__(self, credentials, source_config):
         self.credentials = credentials
         self.source_config = source_config
         self.curr_batch = 0
         self.validate_config()
+        self.db_initialization() # connect to db # create cursor
+        sql_str = self.construct_sql() # construct sql
+        self.execute_sql(sql_str) # execute sql
+        self.itersize = self.cursor.itersize
 
     def get_batch(self):
-        self.curr_batch += 1
-        start_idx = (self.curr_batch - 1) * self.batchsize
-        end_idx = self.curr_batch * self.batchsize
-        return self.dummy_data[start_idx:end_idx]
+        return self.cursor.fetchmany(self.itersize)
 
     def cleanup(self):
         pass
@@ -39,3 +30,23 @@ class SourcePostgres(object):
             raise ChompSourceException("Key 'columns' not present.")
         elif len(self.source_config['columns']) == 0:
             raise ChompSourceException("Value for 'columns' key is empty.")
+
+    def db_initialization(self):
+        connect_string = self.construct_connect_string()
+        self.connection = psycopg2.connect(connect_string)
+        self.cursor = self.connection.cursor('chompetl_named_cursor')
+
+    def construct_sql(self):
+        col_str = ", ".join(self.source_config['columns'])
+        table = self.source_config['table']
+        sql_str = f"SELECT {col_str} FROM {table}"
+        return sql_str
+
+    def execute_sql(self, sql_str):
+        self.cursor.execute(sql_str)
+
+    def construct_connect_string(self):
+        connect_string = ""
+        for (k, v) in self.credentials.items():
+            connect_string += f"{k}={v} "
+        return connect_string
