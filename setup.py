2d3dfe1161bf8e4369471cdab8be8b93012ee64b
Sundaram Vedala <svedala23@gmail.com>
2018-08-08 05:32:59 -0400

    Polish packaging - include psycopg2, exclude tests module

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 setuptools.setup(
     name="source_postgres",
     version="0.0.1",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=[
+        "psycopg2",
+    ],
 )
