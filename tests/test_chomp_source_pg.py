079bbd452b5f3bf13160d149f7787e3f1175a38b
Sundaram Vedala <svedala23@gmail.com>
2018-08-13 16:19:19 -0400

    Add unit test for SourcePostgres construct_connect_string method

--- tests/test_chomp_source_pg.py
+++ tests/test_chomp_source_pg.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import os
 from source_postgres import SourcePostgres
 
@@ -17,6 +18,17 @@ class ExtractTestCase(unittest.TestCase):
         }
         source = SourcePostgres(credentials, source_config)
         self.assertEqual(2000, source.itersize)
+
+    @patch.object(SourcePostgres, '__init__')
+    def test_construct_connect_string(self, mock_init):
+        """Is correct connect string constructed from supplied dictionary?"""
+
+        mock_init.return_value = None
+
+        source_pg_inst = SourcePostgres("", "")
+        source_pg_inst.credentials = { 'somekey1': 'some_value', 'somekey2':'another_value'}
+        expected_string="somekey1=some_value somekey2=another_value "
+        self.assertEqual(expected_string, source_pg_inst.construct_connect_string())
 
 if __name__ == "__main__":
     unittest.main()
