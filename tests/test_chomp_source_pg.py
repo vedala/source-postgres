959c37b0d9df2c368ffe9b92373e7ea90b2d8f8c
Sundaram Vedala <svedala23@gmail.com>
2018-08-14 15:56:30 -0400

    Add test for db_initialization method

--- tests/test_chomp_source_pg.py
+++ tests/test_chomp_source_pg.py
@@ -3,7 +3,6 @@ from unittest.mock import patch, MagicMock
 import os
 from source_postgres import SourcePostgres
 import psycopg2
-from psycopg2 import cursor
 
 class ExtractTestCase(unittest.TestCase):
     """Tests for `extract.py`."""
@@ -43,6 +42,26 @@ class ExtractTestCase(unittest.TestCase):
         source.execute_sql("some_sql_string")
         mock_execute_sql.assert_called_once_with("some_sql_string")
 
+
+    @patch('psycopg2.connect')
+    @patch.object(SourcePostgres,
+                'construct_connect_string', return_value='some_connect_string')
+    @patch.object(SourcePostgres, '__init__')
+    def test_db_initialization(self, mock_init, mock_cons_conn_str,
+                                                          mock_pg_connect):
+        """Is execute on cursor called with sql_str argument?"""
+
+        class MyConnection(object):
+            def cursor(self, cursor_name):
+                return cursor_name
+
+        mock_init.return_value = None
+        mock_pg_connect.return_value = MyConnection()
+        source = SourcePostgres()
+        source.db_initialization()
+        mock_cons_conn_str.assert_called_once_with()
+        mock_pg_connect.assert_called_once_with("some_connect_string")
+        self.assertEqual("chompetl_named_cursor", source.cursor)
 
 if __name__ == "__main__":
     unittest.main()
